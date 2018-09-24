173addc86878805769e2d85b95d2d5bd069e0259
zrzw <31051194+zrzw@users.noreply.github.com>
2018-09-24 12:40:00 +0100

    updated client.c to resolve addresses instead of canonical names

--- sockets/client.c
+++ sockets/client.c
@@ -1,33 +1,37 @@
+/*
+ * Simple client to resolve hostnames
+ */
 #include <sys/types.h>
 #include <sys/socket.h>
 #include <string.h>
 #include <stdio.h>
 #include <stdlib.h>
 #include <netdb.h>
+#include <arpa/inet.h>
 
-int
-main()
+int main()
 {
-        struct addrinfo hints;
-        struct addrinfo *servinfo;
-        memset(&hints, 0, sizeof hints);
-        /* ai_flags settings
-         *  AI_PASSIVE: socket is intended for bind() (server)
-         *
-         */
-        hints.ai_flags = AI_CANONNAME;
-        hints.ai_family = AF_INET; /* Use AF_INET for IPv4 only */
-        hints.ai_socktype = SOCK_STREAM;
-        int status = getaddrinfo("8.8.8.8", "80", &hints, &servinfo);
-
-        if(status != 0){
-                fprintf(stderr, "getaddrinfo error: %s\n", gai_strerror(status));
-                exit(1);
-        }
-        struct addrinfo *p = servinfo;
-        while(p != NULL) {
-                fprintf(stdout, "result: %s\n", p->ai_canonname);
-                p = p->ai_next;
-        }
-        freeaddrinfo(servinfo);
+    struct addrinfo hints;
+    struct addrinfo *servinfo;
+    memset(&hints, 0, sizeof hints);
+    const char* hostname = "google.co.uk";
+    const char* port = "80";
+    hints.ai_family = AF_INET; /* Use AF_INET for IPv4 only */
+    hints.ai_socktype = SOCK_STREAM;
+    int status = getaddrinfo(hostname, port, &hints, &servinfo);
+    if(status != 0){
+        fprintf(stderr, "getaddrinfo error: %s\n", gai_strerror(status));
+        exit(1);
+    }
+    struct addrinfo *p = servinfo;
+    while(p != NULL){
+        char str[INET_ADDRSTRLEN];
+        const char* rc = inet_ntop(p->ai_family,
+            &((struct sockaddr_in*) p->ai_addr)->sin_addr,
+            str, sizeof(str));
+        if(rc != NULL)
+            fprintf(stdout, "result: %s\n", str);
+        p = p->ai_next;
+    }
+    freeaddrinfo(servinfo);
 }
--- sockets/rawdns.c
+++ sockets/rawdns.c
@@ -16,37 +16,32 @@
 int rawdns(const char* hostname);
 void print_hdr_sizes(void);
 
-int
-rawdns(const char* ip_str)
+int rawdns(const char* ip_str)
 {
-        int s = socket(PF_INET, SOCK_RAW, IPPROTO_UDP);
-        if(s == -1)
-        {
-                fprintf(stderr, "Error creating raw socket\n");
-                return -1;
-        }
-        struct sockaddr_in sa;
-        sa.sin_family = AF_INET;
-        int rc = inet_pton(AF_INET, ip_str, &(sa.sin_addr.s_addr));
-        if(rc != 1)
-        {
-                fprintf(stderr, "Error filling sockaddr_in with given IP STR\n");
-                return -1;
-        }
-        sa.sin_port = htons(53);
-        return 0;
+    int s = socket(PF_INET, SOCK_RAW, IPPROTO_UDP);
+    if(s == -1){
+        fprintf(stderr, "Error creating raw socket\n");
+        return -1;
+    }
+    struct sockaddr_in sa;
+    sa.sin_family = AF_INET;
+    int rc = inet_pton(AF_INET, ip_str, &(sa.sin_addr.s_addr));
+    if(rc != 1){
+        fprintf(stderr, "Error filling sockaddr_in with given IP STR\n");
+        return -1;
+    }
+    sa.sin_port = htons(53);
+    return 0;
 }
 
-void
-print_hdr_sizes(void)
+void print_hdr_sizes(void)
 {
-        printf("Size of struct iphdr: %zu\n", sizeof(struct iphdr));
-        printf("Size of struct udphdr: %zu\n", sizeof(struct udphdr));
+    printf("Size of struct iphdr: %zu\n", sizeof(struct iphdr));
+    printf("Size of struct udphdr: %zu\n", sizeof(struct udphdr));
 }
 
-int
-main(int argc, char** argv)
+int main(int argc, char** argv)
 {
-        print_hdr_sizes();
-        return rawdns("4.4.4.4");
+    print_hdr_sizes();
+    return rawdns("4.4.4.4");
 }
